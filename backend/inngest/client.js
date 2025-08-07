2ea0b7107bdcef858492b8fdfe88cf27d380949f
Anurag Raj <adityanv8@gmail.com>
2025-08-08 02:20:24 +0530

    add event key

--- backend/inngest/client.js
+++ backend/inngest/client.js
@@ -1,5 +1,5 @@
 import {Inngest} from 'inngest'
 
-export const inngest = new Inngest({id:"ticketing-system"})
+export const inngest = new Inngest({id:"ticketing-system",name:"Myapp",eventKey:process.env.INNGEST_EVENT_KEY,})
 //All the function keep togeather under this "ticketing-system" banner
 
